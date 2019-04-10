0772214b2b8cec65adec55231a0016d6156368f8
Lindson França <l1nds0n@hotmail.com>
2019-04-10 15:13:03 -0300

    Adicionado controlador genérico e alterações aplicadas aos controladores.

--- src/main/java/br/senai/rn/locadora/controllers/FilmeController.java
+++ src/main/java/br/senai/rn/locadora/controllers/FilmeController.java
@@ -1,36 +1,17 @@
 package br.senai.rn.locadora.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.senai.rn.locadora.models.Filme;
-import br.senai.rn.locadora.services.FilmeService;
 
 @Controller
 @RequestMapping("/filme")
-public class FilmeController {
+public class FilmeController extends AbstractController<Filme>{
 
-	@Autowired
-	private FilmeService service;
-	
-	@GetMapping
-	public String index(Model model) {
-		Filme filme = new Filme();
-		List<Filme> filmes = service.obterTodos();
-		model.addAttribute("filme", filme);
-		model.addAttribute("filmes", filmes);
-		return "filme/index";
-	}
-	
-	@PostMapping
-	public String salvar(Filme filme) {
-		service.salvar(filme);
-		return "redirect:/filme/";
+	@Override
+	public Class<Filme> getClassType() {
+		// TODO Auto-generated method stub
+		return Filme.class;
 	}
 }

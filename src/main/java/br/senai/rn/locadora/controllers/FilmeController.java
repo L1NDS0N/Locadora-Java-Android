d46df34459a8b74b2ef9e394a12320944716fc62
Lindson França <l1nds0n@hotmail.com>
2019-04-10 16:39:58 -0300

    Rever

--- src/main/java/br/senai/rn/locadora/controllers/FilmeController.java
+++ src/main/java/br/senai/rn/locadora/controllers/FilmeController.java
@@ -1,9 +1,15 @@
 package br.senai.rn.locadora.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.senai.rn.locadora.models.Categoria;
 import br.senai.rn.locadora.models.Filme;
+import br.senai.rn.locadora.services.CategoriaService;
 
 @Controller
 @RequestMapping("/filme")
@@ -11,7 +17,22 @@ public class FilmeController extends AbstractController<Filme>{
 
 	@Override
 	public Class<Filme> getClassType() {
-		// TODO Auto-generated method stub
 		return Filme.class;
+	}
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@Override
+	public String index(Model model) {
+		List<Categoria> categorias = categoriaService.obterTodos();
+		model.addAttribute("categoriaLista", categorias);
+		return super.index(model);
+	}
+	
+	@Override
+	public String editar(Long id, Model model) {
+		List<Categoria> categorias = categoriaService.obterTodos();
+		model.addAttribute("categoriaLista", categorias);
+		return super.editar(id, model);
 	}
 }
